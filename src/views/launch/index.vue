4a983521a78abb494534002ad8ed63775b134878
CuberLuo <1501858187@qq.com>
2022-06-26 17:34:18 +0800

    完成了利用fetch进行请求的初步测试

--- src/views/launch/index.vue
+++ src/views/launch/index.vue
@@ -1,7 +1,35 @@
 <template>
   <div>启动页面</div>
+  <button @click="getUser">获取所有用户信息</button>
+  <button @click="handleLogin">登录</button>
 </template>
 
-<script setup></script>
+<script setup>
+const getUser = async () => {
+  const response = await fetch('http://localhost:3000', {
+    method: 'GET'
+  })
+  const data = await response.json()
+
+  console.log(data)
+}
+
+const handleLogin = async () => {
+  const response = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'user3',
+      password: '*ZJUT&123'
+    })
+  })
+
+  const data = await response.json()
+
+  console.log(data)
+}
+</script>
 
 <style scoped></style>
